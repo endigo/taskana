c679b3e44e8dae3d127aad700f056daad863a8c0
endigo <endigo.18@gmail.com>
2025-02-18 13:39:27 +0800

    fix  button style

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -7,10 +7,8 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-[24px] font-medium">{APP_NAME}</h1>
 
-      <Button asChild variant="default">
-        <Link href="/tasks" className="px-12 py-6 rounded-[32px]">
-          Go To Tasks
-        </Link>
+      <Button asChild variant="default" className="px-12 py-6 rounded-[32px]">
+        <Link href="/tasks">Go To Tasks</Link>
       </Button>
     </div>
   );
